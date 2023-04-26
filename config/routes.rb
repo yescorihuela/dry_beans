e8fecd46e280bf975030c8217111167743b2fbc7
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2023-04-26 18:54:05 -0400

    Remove useless route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :routes, only: [:create, :show] do
-        resources :trips, only: [:create, :show] do
+        resources :trips, only: [:create] do
           member do
             patch 'complete', to: 'trips#complete'
           end
